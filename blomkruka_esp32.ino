fff28386a6b171421a7990afd8237ed95a28dee3
Robin Ellingsen <70570646+ascoolarobban@users.noreply.github.com>
2021-04-23 18:56:48 +0200

    case switch

--- blomkruka_esp32.ino
+++ blomkruka_esp32.ino
@@ -5,11 +5,11 @@
 int M_Sensor = 34;
 int Moisture = 0;
 int moistPercent = 0;
-int newStatus;
-int oldStatus;
+
+int oldStatus = 0;
 
 const int dryValue = 4096;
-const int wetValue = 2865;
+const int wetValue = 2800;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -37,75 +37,96 @@ void setup()
 
 
 }
-void setStatus(int newStatus, int oldStatus) {
+void setStatus(int newStatus) {
   if (oldStatus != newStatus) {
+    Serial.println("old: ");
+    Serial.println(oldStatus);
+    Serial.println("New: ");
+    Serial.println(newStatus);
     oldStatus = newStatus;
-    switch (newStatus) {
-      case 1:
-        client.publish(mqttMoist, "DRY");
-        client.publish(mqttMoist, String (moistPercent).c_str(), true);
-        break;
-      case 2:
-        client.publish(mqttMoist, "MOIST");
-        client.publish(mqttMoist, String (moistPercent).c_str(), true);
-        break;
+    Serial.println("AFTER SWITCH");
+    Serial.println("old: ");
+    Serial.println(oldStatus);
+    Serial.println("New: ");
+    Serial.println(newStatus);
+    delay(5000);
 
-      case 3:
-        client.publish(mqttMoist, "WET");
-        client.publish(mqttMoist, String (moistPercent).c_str(), true);
-        break;
+
+
+    if (oldStatus == 1) {
+
+      Serial.println("case 1 DRY");
+      client.publish(mqttMoist, "DRY");
+      delay(500);
+      client.publish(mqttMoist, String (moistPercent).c_str(), true);
+      delay(5000);
     }
-
-  }
-}
-
-  void loop()
-  {
-
-    Moisture = analogRead(M_Sensor);
-    moistPercent = map(Moisture, dryValue, wetValue, 0, 100);
-
-    Serial.println(Moisture);
-    delay(1000);
-    if (Moisture > 3950)  // for dry soil
+    else if (oldStatus == 2)
     {
 
-      newStatus = 1;
-      setStatus(newStatus,oldStatus);
-      Serial.println ("DRY SOIL");
-      Serial.print(moistPercent);
-      Serial.println("%");
-
-      digitalWrite(Relay, HIGH);
-      delay(4000);
-      client.publish(mqttMoist, "watered");
-      digitalWrite(Relay, LOW);
-
+      Serial.println("case 2 Moist");
+      client.publish(mqttMoist, "MOIST");
+      delay(500);
+      client.publish(mqttMoist, String (moistPercent).c_str(), true);
+      delay(5000);
     }
-    else if (Moisture >= 3000 && Moisture <= 3950) //for Moist Soil
-    {
-      newStatus = 2;
-      setStatus(newStatus,oldStatus);
-      digitalWrite(Relay, LOW);
 
-      Serial.println("MOIST SOIL");
-      Serial.print(moistPercent);
-      Serial.println("%");
-
-      client.publish(mqttMoist, "Moist Soil");
+    else if (oldStatus == 3) {
+      Serial.println("case 3 Wet");
+      client.publish(mqttMoist, "WET");
+      delay(500);
       client.publish(mqttMoist, String (moistPercent).c_str(), true);
     }
+  }
 
-    else
-    {
-      newStatus = 3;
-      setStatus(newStatus,oldStatus);
-      digitalWrite(Relay, LOW);
+}
 
-      Serial.println("WET");
-      Serial.print(moistPercent);
-      Serial.println("%");
 
-    }
+void loop()
+{
+  int newStatus = 0;
+
+  Moisture = analogRead(M_Sensor);
+  moistPercent = map(Moisture, dryValue, wetValue, 0, 100);
+
+  Serial.println(Moisture);
+  delay(1000);
+  if (Moisture > 3950)  // for dry soil
+  {
+
+    newStatus = 1;
+    setStatus(newStatus);
+    Serial.println ("DRY SOIL");
+    Serial.print(moistPercent);
+    Serial.println("%");
+
+    digitalWrite(Relay, HIGH);
+    delay(4000);
+    client.publish(mqttMoist, "watered");
+    digitalWrite(Relay, LOW);
 
   }
+  else if (Moisture >= 3000 && Moisture <= 3950) //for Moist Soil
+  {
+    newStatus = 2;
+    setStatus(newStatus);
+    digitalWrite(Relay, LOW);
+
+    Serial.println("MOIST SOIL");
+    Serial.print(moistPercent);
+    Serial.println("%");
+  }
+
+  else
+  {
+    newStatus = 3;
+    setStatus(newStatus);
+    digitalWrite(Relay, LOW);
+    
+    Serial.println("WET");
+    Serial.print(moistPercent);
+    Serial.println("%");
+
+  }
+
+}
